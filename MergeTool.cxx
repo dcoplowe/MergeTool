365c995454764fb1acccd2140c43673edbd86c2e
dcoplowe <david.coplowe@physics.ox.ac.uk>
2017-01-21 15:23:57 +0000

    Fixes

--- MergeTool.cxx
+++ MergeTool.cxx
@@ -458,8 +458,8 @@ double MergeTool::getTChainPOT(TChain * ch, const char* branch)
         
         if(lUsed) delete lUsed;//Added 210117
         
-        delete t;//Added 210117
         f->Close();
+        delete t;//Added 210117
         delete f;
     }
     
