6649de63f13ee8c6c7d0062a96861ba1a0947e0d
dcoplowe <david.coplowe@physics.ox.ac.uk>
2017-01-22 00:54:07 +0000

    Mem issue

--- MergeTool.cxx
+++ MergeTool.cxx
@@ -457,12 +457,12 @@ double MergeTool::getTChainPOT(TChain * ch, const char* branch)
         if(lUsed)         sumPOTUsed+=lUsed->GetValue();
         cout << "MergeTool::getTChainPOT(TChain * ch, const char* branch) :: Looping. " << chEl->GetTitle() << endl;
 
-//        if(lUsed) delete lUsed;//Added 210117
-//        f.Close();
-        //delete t;//Added 210117
+        if(lUsed) delete lUsed;//Added 210117
+        f.Close();
+        delete t;//Added 210117
     }
     
-    delete fileElements;//Added 210117
+//    delete fileElements;//Added 210117
     
     cout << "MergeTool::getTChainPOT(TChain * ch, const char* branch) :: delete fileElements. " << chEl->GetTitle() << endl;
 
