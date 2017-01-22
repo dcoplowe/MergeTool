22e768ad211b01c8d78e02ece18fa9c75050bfd0
dcoplowe <david.coplowe@physics.ox.ac.uk>
2017-01-22 00:56:17 +0000

    Mem issue

--- MergeTool.cxx
+++ MergeTool.cxx
@@ -459,7 +459,7 @@ double MergeTool::getTChainPOT(TChain * ch, const char* branch)
 
         if(lUsed) delete lUsed;//Added 210117
         f.Close();
-        delete t;//Added 210117
+//        delete t;//Added 210117
     }
     
 //    delete fileElements;//Added 210117
